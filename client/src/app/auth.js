01246c81fb28ac2db1fc1a2463f96b0370664cc0
Soumodeep-Ganguly <soumodeep.ganguly.007@gmail.com>
2024-07-29 20:26:50 +0530

    modified-auth

--- client/src/app/auth.js
+++ client/src/app/auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Navigate } from 'react-router-dom';
 
@@ -7,82 +7,113 @@ export const AuthContext = React.createContext();
 
 export function AuthProvider({ children }){
 
-    const cache = JSON.parse(localStorage.getItem('user'));
-    const [user, setUser] = useState(cache);
-    const [dark, setDark] = useState(localStorage.getItem('darkMode'))
-  
-    function signin(res){
-      if (res.data){
-  
-        localStorage.setItem('user', JSON.stringify(res.data));
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
-  
-        return window.location = '/'
-  
-      }
+  const cache = JSON.parse(localStorage.getItem('user'));
+  const [user, setUser] = useState(cache);
+  const [dark, setDark] = useState(() => {
+    const savedMode = localStorage.getItem('color-theme');
+    return savedMode === 'dark' || (!savedMode && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  })
+
+  useEffect(() => {
+    if (dark) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('color-theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('color-theme', 'light');
     }
-  
-    async function signout(){
-  
-      localStorage.clear();
-      return window.location = '/signin'
-  
+  }, [dark]);
+
+  function signin(res){
+    if (res.data){
+
+      localStorage.setItem('user', JSON.stringify(res.data));
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
+
+      return window.location = '/'
+
     }
-  
-    function update(data){
-  
-      if (localStorage.getItem('user')){
-  
-        let user = JSON.parse(localStorage.getItem('user'));
-        for (let key in data){
-  
-          if (Array.isArray(data[key])){
-         
-            user[key] = data[key]
-  
-          }
-          else if (typeof data[key] === 'object'){
-            for (let innerKey in data[key]){
-  
-              user[key][innerKey] = data[key][innerKey]
-  
-            }
-          }
-          else {
-            
-            user[key] = data[key];
-  
+  }
+
+  async function signout(){
+
+    localStorage.clear();
+    return window.location = '/signin'
+
+  }
+
+  function update(data){
+
+    if (localStorage.getItem('user')){
+
+      let user = JSON.parse(localStorage.getItem('user'));
+      for (let key in data){
+
+        if (Array.isArray(data[key])){
+        
+          user[key] = data[key]
+
+        }
+        else if (typeof data[key] === 'object'){
+          for (let innerKey in data[key]){
+
+            user[key][innerKey] = data[key][innerKey]
+
           }
         }
-  
-        localStorage.setItem('user', JSON.stringify(user));
-        setUser(user);
-  
-      }
-    }
-  
-    return (
-        <AuthContext.Provider value={{
-                dark, setDark,
-                user: user,
-                signin: signin,
-                signout: signout,
-                update: update
+        else {
+          
+          user[key] = data[key];
 
-            }}
-        > 
-            {children}
-        </AuthContext.Provider>
-    );
+        }
+      }
+
+      localStorage.setItem('user', JSON.stringify(user));
+      setUser(user);
+
+    }
+  }
+
+  return (
+      <AuthContext.Provider value={{
+              dark, setDark,
+              user: user,
+              signin: signin,
+              signout: signout,
+              update: update
+
+          }}
+      > 
+          {children}
+      </AuthContext.Provider>
+  );
 }
 
 export function PrivateRoute(props){  
-    // check user exists
-    const user = JSON.parse(localStorage.getItem('user'));
+  // check user exists
+  const { user, signout } = useContext(AuthContext);
 
-    if (user?.token){ 
-        return props.children
-    }
-    return <Navigate to='/signin' />;
+  useEffect(() => {
+    const interceptor = axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response && error.response.status === 401) {
+          // Clear user context and redirect to sign-in page
+          signout();
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      // Eject the interceptor when the component unmounts
+      axios.interceptors.response.eject(interceptor);
+    };
+  }, [signout]);
+
+  if (user?.token){ 
+    return props.children
+  }
+  return <Navigate to='/signin' />;
 
 }
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,7 +5,10 @@ module.exports.authenticateToken = (req, res, next) => {
     if (!token) return res.status(401).json({ error: 'Unauthorized' });
 
     jwt.verify(token, process.env.JWT_TOKEN_SECRET || "mern-stack-boiler-plate", (err, user) => {
-        if (err) return res.status(403).json({ error: 'Forbidden' });
+        if (err) {
+            if (err.name === 'TokenExpiredError') return res.status(401).json({ error: 'Token expired' });
+            return res.status(403).json({ error: 'Forbidden' });
+        }
         req.user = user;
         next();
     });
